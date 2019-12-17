92d232f2ba77f7a5025696ae1b98f501c8c2d909
astudina <nastya.astudina@mail.ru>
2019-12-18 01:31:28 +0300

    tests + report + examples

--- src/edgeConnectivity/Graph.java
+++ src/edgeConnectivity/Graph.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-class Graph {
+public class Graph {
     int numberOfStrings;
     int[][] matrix, sourceNetwork;
     private int[][] residualNetwork;
